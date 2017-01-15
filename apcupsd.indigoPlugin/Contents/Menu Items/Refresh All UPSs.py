e6ac3610ff8324e00572231f8d7e69bc56c967e2
Marty Skinner <vendor.github@myspecialdomain.com>
2017-01-14 22:12:25 -0500

    removal of a confusing comment

--- apcupsd.indigoPlugin/Contents/Menu Items/Refresh All UPSs.py
+++ apcupsd.indigoPlugin/Contents/Menu Items/Refresh All UPSs.py
@@ -5,6 +5,5 @@ prId = "com.berkinet.apcupsd"
 apcupsdPlugin = indigo.server.getPlugin(prId)
 for device in indigo.devices.iter(prId):
     if device.enabled:
-        # indigo.server.log("Refreshng data for device Id %s" % (tstat.name), type='Proliphix Plugin')
         apcupsdPlugin.executeAction("readApcupsd", deviceId=device.id)
         indigo.server.log("Refreshed data for device Id %s" % (device.name), type='apcupsd')
