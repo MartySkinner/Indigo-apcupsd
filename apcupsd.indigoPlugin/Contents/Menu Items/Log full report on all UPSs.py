79a64b73ae6029a0e73e6207712bfa14da775fd2
Marty Skinner <vendor.github@myspecialdomain.com>
2017-02-13 22:40:25 -0500

    change plugin Id

--- apcupsd.indigoPlugin/Contents/Menu Items/Log full report on all UPSs.py
+++ apcupsd.indigoPlugin/Contents/Menu Items/Log full report on all UPSs.py
@@ -1,9 +1,14 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
-prId = "com.berkinet.apcupsd"
+prId = "com.martys.apcupsd"
 apcupsdPlugin = indigo.server.getPlugin(prId)
+cnt = 0
 for device in indigo.devices.iter(prId):
     if device.enabled:
         apcupsdPlugin.executeAction("logStatusReport", deviceId=device.id)
-        indigo.server.log("Wrote full status report for all UPSs to the Indigo log", type='apcupsd')
+        indigo.server.log("End of full status report for all enabled UPSs", type='apcupsd')
+        cnt = cnt + 1
+if cnt == 0:
+        indigo.server.log("Did not find any matching enabled UPS devices", type='apcupsd')
+
--- apcupsd.indigoPlugin/Contents/Menu Items/Refresh All UPSs.py
+++ apcupsd.indigoPlugin/Contents/Menu Items/Refresh All UPSs.py
@@ -1,9 +1,13 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
-prId = "com.berkinet.apcupsd"
+prId = "com.martys.apcupsd"
 apcupsdPlugin = indigo.server.getPlugin(prId)
+cnt = 0
 for device in indigo.devices.iter(prId):
     if device.enabled:
         apcupsdPlugin.executeAction("readApcupsd", deviceId=device.id)
-        indigo.server.log("Refreshed data for device Id %s" % (device.name), type='apcupsd')
+        indigo.server.log("Refreshed data for device name %s" % (device.name), type='apcupsd')
+        cnt = cnt + 1
+if cnt == 0:
+        indigo.server.log("Did not find any matching enabled UPS devices", type='apcupsd')
